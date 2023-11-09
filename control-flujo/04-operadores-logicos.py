157ab34884b5920759b6a5a06bd44df68240d93b
Jpepetrueno <jpernia15@gmail.com>
2023-11-09 10:56:14 -0400

    Probando GitHub Desktop

---
+++ control-flujo/04-operadores-logicos.py
@@ -0,0 +1,14 @@
+# and, or, not
+
+gas = False
+encendido = True
+edad = 18
+
+if gas and encendido and edad > 17:
+    print("Estoy encendido 1")
+
+if gas or encendido:
+    print("Estoy encendido 2")
+
+if not gas and encendido:
+    print("Estoy encendido 3")
